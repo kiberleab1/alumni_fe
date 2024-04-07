45278504e478200c6dff8f15be2d31f68a660ce4
Biruk Abate <abate3010@gmail.com>
2024-04-07 17:26:51 +0300

    Make table componenet, pagination working now

--- src/pages/Admins.jsx
+++ src/pages/Admins.jsx
@@ -1,11 +1,12 @@
 import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/24/outline';
 import React from 'react';
+import Table from '../components/Table/Table';
 
 const people = [
     {
         name: 'Jane Cooper',
         title: 'Regional Paradigm Technician',
-        department: 'Optimization',
+        institute: 'Optimization',
         role: 'Admin',
         email: 'jane.cooper@example.com',
         image: 'https://bit.ly/33HnjK0',
@@ -13,7 +14,7 @@ const people = [
     {
         name: 'John Doe',
         title: 'Regional Paradigm Technician',
-        department: 'Optimization',
+        institute: 'Optimization',
         role: 'Tester',
         email: 'john.doe@example.com',
         image: 'https://bit.ly/3I9nL2D',
@@ -21,7 +22,7 @@ const people = [
     {
         name: 'Veronica Lodge',
         title: 'Regional Paradigm Technician',
-        department: 'Optimization',
+        institute: 'Optimization',
         role: ' Software Engineer',
         email: 'veronica.lodge@example.com',
         image: 'https://bit.ly/3vaOTe1',
@@ -29,63 +30,63 @@ const people = [
     {
         name: 'Veronica Lodge',
         title: 'Regional Paradigm Technician',
-        department: 'Optimization',
+        institute: 'Optimization',
         role: ' Software Engineer',
         email: 'veronica.lodge@example.com',
         image: 'https://bit.ly/3vaOTe1',
     }, {
         name: 'Veronica Lodge',
         title: 'Regional Paradigm Technician',
-        department: 'Optimization',
+        institute: 'Optimization',
         role: ' Software Engineer',
         email: 'veronica.lodge@example.com',
         image: 'https://bit.ly/3vaOTe1',
     }, {
         name: 'Veronica Lodge',
         title: 'Regional Paradigm Technician',
-        department: 'Optimization',
+        institute: 'Optimization',
         role: ' Software Engineer',
         email: 'veronica.lodge@example.com',
         image: 'https://bit.ly/3vaOTe1',
     }, {
         name: 'Veronica Lodge',
         title: 'Regional Paradigm Technician',
-        department: 'Optimization',
+        institute: 'Optimization',
         role: ' Software Engineer',
         email: 'veronica.lodge@example.com',
         image: 'https://bit.ly/3vaOTe1',
     }, {
         name: 'Veronica Lodge',
         title: 'Regional Paradigm Technician',
-        department: 'Optimization',
+        institute: 'Optimization',
         role: ' Software Engineer',
         email: 'veronica.lodge@example.com',
         image: 'https://bit.ly/3vaOTe1',
     }, {
         name: 'Veronica Lodge',
         title: 'Regional Paradigm Technician',
-        department: 'Optimization',
+        institute: 'Optimization',
         role: ' Software Engineer',
         email: 'veronica.lodge@example.com',
         image: 'https://bit.ly/3vaOTe1',
     }, {
         name: 'Veronica Lodge',
         title: 'Regional Paradigm Technician',
-        department: 'Optimization',
+        institute: 'Optimization',
         role: ' Software Engineer',
         email: 'veronica.lodge@example.com',
         image: 'https://bit.ly/3vaOTe1',
     }, {
         name: 'Veronica Lodge',
         title: 'Regional Paradigm Technician',
-        department: 'Optimization',
+        institute: 'Optimization',
         role: ' Software Engineer',
         email: 'veronica.lodge@example.com',
         image: 'https://bit.ly/3vaOTe1',
     }, {
         name: 'Veronica Lodge',
         title: 'Regional Paradigm Technician',
-        department: 'Optimization',
+        institute: 'Optimization',
         role: ' Software Engineer',
         email: 'veronica.lodge@example.com',
         image: 'https://bit.ly/3vaOTe1',
@@ -93,7 +94,18 @@ const people = [
     // More people...
 ];
 
+const headers = ['name','email','institute','title','role','status'];
 
+const statusOptions = ['active', 'deactivated', 'pending'];
+
+function getRandomStatus() {
+    return statusOptions[Math.floor(Math.random() * statusOptions.length)];
+}
+
+const updatedPeople = people.map(person => ({
+    ...person,
+    status: getRandomStatus()
+}));
 
 export default function Admins() {
     return (
@@ -115,164 +127,7 @@ export default function Admins() {
                 </div>
             </div>
             <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-                <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-                    <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-                        <div className="min-w-full">
-                            <div className="overflow-x-auto">
-                                <div className="table-container" style={{ maxHeight: "750px" }}>
-                                    <table className="min-w-full divide-y divide-gray-200">
-                                        <thead className="sticky top-0 bg-gray-50 z-10">
-                                            <tr>
-                                                <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-900 uppercase tracking-wider">
-                                                    Name
-                                                </th>
-                                                <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-900 uppercase tracking-wider">
-                                                    Email
-                                                </th>
-                                                <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-900 uppercase tracking-wider">
-                                                    Institute
-                                                </th>
-                                                <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-900 uppercase tracking-wider">
-                                                    Title
-                                                </th>
-                                                <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-900 uppercase tracking-wider">
-                                                    Status
-                                                </th>
-                                                <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-900 uppercase tracking-wider">
-                                                    Role
-                                                </th>
-                                                <th scope="col" className="relative px-6 py-3">
-                                                    <span className="sr-only">Edit</span>
-                                                </th>
-                                            </tr>
-                                        </thead>
-                                        <tbody className="bg-white divide-y divide-gray-200">
-                                            {people.map(person => (
-                                                <tr key={person.email}>
-                                                    <td className="px-6 py-4 whitespace-nowrap">
-                                                        <div className="flex items-start">
-                                                            <div className="flex-shrink-0 h-10 w-10">
-                                                                <img className="h-10 w-10 rounded-full" src={person.image} alt="" />
-                                                            </div>
-                                                            <div className="ml-4">
-                                                                <div className="text-sm font-medium text-gray-900">{person.name}</div>
-                                                            </div>
-                                                        </div>
-                                                    </td>
-                                                    <td className="px-6 py-4 whitespace-nowrap text-start">
-                                                        <div className="text-sm text-gray-900">{person.email}</div>
-                                                    </td>
-                                                    <td className="px-6 py-4 whitespace-nowrap text-start">
-                                                        <div className="text-sm text-gray-900">{person.title}</div>
-                                                    </td>
-                                                    <td className="px-6 py-4 whitespace-nowrap text-start">
-                                                        <div className="text-sm text-gray-900">{person.department}</div>
-                                                    </td>
-                                                    <td className="px-6 py-4 whitespace-nowrap text-start">
-                                                        <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
-                                                            Active
-                                                        </span>
-                                                    </td>
-                                                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900 text-start">
-                                                        {person.role}
-                                                    </td>
-                                                    <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium ">
-                                                        <a href="#" className="text-indigo-600 hover:text-indigo-900">
-                                                            Edit
-                                                        </a>
-                                                    </td>
-                                                </tr>
-                                            ))}
-                                        </tbody>
-                                    </table>
-
-                                </div>
-                            </div>
-                        </div>
-                        <div className="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
-                            <div className="flex flex-1 justify-between sm:hidden">
-                                <a
-                                    href="#"
-                                    className="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
-                                >
-                                    Previous
-                                </a>
-                                <a
-                                    href="#"
-                                    className="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
-                                >
-                                    Next
-                                </a>
-                            </div>
-                            <div className="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
-                                <div>
-                                    <p className="text-sm text-gray-700">
-                                        Showing <span className="font-medium">1</span> to <span className="font-medium">10</span> of{' '}
-                                        <span className="font-medium">97</span> results
-                                    </p>
-                                </div>
-                                <div>
-                                    <nav className="isolate inline-flex -space-x-px rounded-md shadow-sm" aria-label="Pagination">
-                                        <a
-                                            href="#"
-                                            className="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
-                                        >
-                                            <span className="sr-only">Previous</span>
-                                            <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" />
-                                        </a>
-                                        <a
-                                            href="#"
-                                            aria-current="page"
-                                            className="relative z-10 inline-flex items-center bg-indigo-600 px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                                        >
-                                            1
-                                        </a>
-                                        <a
-                                            href="#"
-                                            className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
-                                        >
-                                            2
-                                        </a>
-                                        <a
-                                            href="#"
-                                            className="relative hidden items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 md:inline-flex"
-                                        >
-                                            3
-                                        </a>
-                                        <span className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-700 ring-1 ring-inset ring-gray-300 focus:outline-offset-0">
-                                            ...
-                                        </span>
-                                        <a
-                                            href="#"
-                                            className="relative hidden items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 md:inline-flex"
-                                        >
-                                            8
-                                        </a>
-                                        <a
-                                            href="#"
-                                            className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
-                                        >
-                                            9
-                                        </a>
-                                        <a
-                                            href="#"
-                                            className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
-                                        >
-                                            10
-                                        </a>
-                                        <a
-                                            href="#"
-                                            className="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
-                                        >
-                                            <span className="sr-only">Next</span>
-                                            <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
-                                        </a>
-                                    </nav>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <Table data={updatedPeople} headers={headers} />
             </div>
         </div>
 
