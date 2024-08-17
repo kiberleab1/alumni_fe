dead295fef740fd0cf2077a348176dfa75cde93b
z <zemelatmen@gmail.com>
2024-08-17 19:49:32 +0300

    add new  stories card

--- src/pages/home/podcast/AlumniPodcast.jsx
+++ src/pages/home/podcast/AlumniPodcast.jsx
@@ -17,44 +17,55 @@ const podcastList = [
     discription:
       " Every utility class in Tailwind can be applied conditionally at different breakpoints, which makes it a piece of cake to build complex responsive interfaces without ever leaving your HTML.",
   },
+  {
+    topic: "Monetization - Episode 2",
+    discription:
+      " Every utility class in Tailwind can be applied conditionally at different breakpoints, which makes it a piece of cake to build complex responsive interfaces without ever leaving your HTML.",
+  },
 ];
 const AlumniPodcast = () => {
   return (
-    <div className="min-h-screen  flex space-x-18  flex-wrap gap-4 space-x-8  items-center justify-center text-black ">
-      {podcastList.map((val, index) => {
-        return (
-          <div className="flex space-x-36     items-center justify-center text-black  m-1 ">
-            <div
-              className="flex  max-w-lg rounded-xl m-10 "
-              style={{ boxShadow: "-50px 30px #7c7d7c" }}
-            >
+    <div>
+      <div className="flex  justify-center mt-4 ">
+        <h1 className="title font-sans"> playlist</h1>
+      </div>
+
+      <div className=" flex  space-x-18  flex-wrap gap-4 space-x-8  items-center justify-center text-black  mt-8 mb-8">
+        {podcastList.map((val, index) => {
+          return (
+            <div className="flex space-x-36     items-center justify-center text-black  m-1 ">
               <div
-                className="max-h-lg  rounded-xl max-w-lg bg-gray-900 p-2"
-                style={{ boxShadow: "60px -40px #ebf0f0" }}
+                className="flex  max-w-lg rounded-xl m-10 "
+                style={{ boxShadow: "-50px 30px #ebf0f0" }}
               >
-                <iframe
-                  // width="500"
-                  height="315"
-                  src="https://www.youtube-nocookie.com/embed/4j7Fa2PCMow?si=HKvw1aIpqyJQwVb1"
-                  title="YouTube video player"
-                  frameborder="0"
-                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                  referrerpolicy="strict-origin-when-cross-origin"
-                  allowfullscreen="true"
-                  className="rounded-xl min-w-[100%]"
-                ></iframe>
-                <div className="flex flex-col text-left pl-4 text-gray-50 ">
-                  <h2 className="title text-xl text-white">{val.topic}</h2>
-                  <p className="text-left over flow scroll ">
-                    {val.discription}
-                  </p>
+                <div
+                  className="max-h-lg  rounded-xl max-w-lg bg-gray-900 p-2"
+                  style={{ boxShadow: "60px -40px #7c7d7c " }}
+                >
+                  <iframe
+                    // width="500"
+                    height="315"
+                    src="https://www.youtube-nocookie.com/embed/4j7Fa2PCMow?si=HKvw1aIpqyJQwVb1"
+                    title="YouTube video player"
+                    frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    referrerpolicy="strict-origin-when-cross-origin"
+                    allowfullscreen="true"
+                    className="rounded-xl min-w-[100%]"
+                  ></iframe>
+                  <div className="flex flex-col text-left pl-4 text-gray-50 ">
+                    <h2 className="title text-xl text-white">{val.topic}</h2>
+                    <p className="text-left over flow scroll ">
+                      {val.discription}
+                    </p>
+                  </div>
+                  <a href="https://www.youtube-nocookie.com/embed/4j7Fa2PCMow?si=HKvw1aIpqyJQwVb1"></a>
                 </div>
-                <a href="https://www.youtube-nocookie.com/embed/4j7Fa2PCMow?si=HKvw1aIpqyJQwVb1"></a>
               </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
     </div>
   );
 };
--- src/views/custom-components/sections/featureComponent.jsx
+++ src/views/custom-components/sections/featureComponent.jsx
@@ -1,5 +1,6 @@
+import { GiFlagObjective } from "react-icons/gi";
 import { Card, CardBody, Col, Container, Row } from "reactstrap";
-
+import "./style/story.css";
 import img1 from "src/assets/images/features/feature13/img1.jpg";
 import img2 from "src/assets/images/features/feature13/img2.jpg";
 import img3 from "src/assets/images/features/feature13/img3.jpg";
@@ -23,7 +24,44 @@ const FeatureComponent = () => {
         </Container>
       </div>
       <div className="min-h-screen flex flex-wrap gap-[8%] items-center justify-center text-black">
-        <div className="relative flex md:flex  lg:items-start min-h-[300px] bg-gray-800 rounded-xl max-w-md md:max-w-2xl  ">
+        <div className="relative flex   md:items-start min-h-[300px] bg-red-800 rounded-xl max-w-md md:max-w-2xl  ">
+          <div className="blog-slider">
+            <div className="blog-slider__wrp swiper-wrapper">
+              <div className="blog-slider__item swiper-slide">
+                <div className="blog-slider__img">
+                  <img src={img1} alt="" />
+                </div>
+                <div className="__content">
+                  <span className="blog-slider__code">26 December 2019</span>
+                  <div className="blog-slider__title">Lorem Ipsum Dolor</div>
+                  <div className="blog-slider__text">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Recusandae voluptate repellendus magni illo ea animi?
+                  </div>
+                  <a href="#" className="blog-slider__button">
+                    READ MORE
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* <div className="absolute w-[250px] h-full md:relative h-24 md:flex-block top-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2  md:left-[-0px] md:top-1/2 md:translate-y-[60%]  ">
+            <img src={img1} className="object-cover rounded-xl " />
+          </div> */}
+          {/* <div className="absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-4  ">
+            <img src={img1} />
+          </div> */}
+          {/* <div className="  flex flex-col ml-[-130px] w-[80%] md:justify- text-start  text-white  bg-yellow-900 items-start">
+            <h2 className="text-xl font-bold text-white ">Alumni Podcast</h2>
+            <h5 className="subtitle mb-3">phd</h5>
+            <p className="text-gray-50">
+              This is some descriptive text that goes next to the image. The
+              text will remain inline and will not overlap with the image.
+            </p>
+          </div> */}
+        </div>
+        {/* <div className="relative flex md:flex  lg:items-start min-h-[300px] bg-gray-800 rounded-xl max-w-md md:max-w-2xl  ">
           <div className="md:shrink-0 m-auto md:bg-red-500">
             <img
               src={img1}
@@ -39,25 +77,9 @@ const FeatureComponent = () => {
               text will remain inline and will not overlap with the image.
             </p>
           </div>
-        </div>
-        <div className="relative flex md:flex  lg:items-start min-h-[300px] bg-gray-800 rounded-xl max-w-md md:max-w-2xl  ">
-          <div className="md:shrink-0 m-auto md:bg-red-500">
-            <img
-              src={img1}
-              className="object-cover absolute h-full left-[-70px] top-1/2 transform -translate-y-1/2 h-[150px] lg:h-[200px] w-[150px] lg:w-[200px] rounded-xl bg-red-400 lg:mb-4 mb-4"
-            />
-          </div>
-
-          <div className=" w-[80%] text-start p-3 mt-4 text-white ">
-            <h2 className="text-xl font-bold text-white ">Alumni Podcast</h2>
-            <h5 className="subtitle mb-3">phd</h5>
-            <p className="text-gray-50">
-              This is some descriptive text that goes next to the image. The
-              text will remain inline and will not overlap with the image.
-            </p>
-          </div>
-        </div>
+        </div> */}
       </div>
+
       {/* <div className="spacer feature4">
         <Container>
           <Row className="justify-content-center">
