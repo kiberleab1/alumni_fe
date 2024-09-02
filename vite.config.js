845738ae8c06e3ea88c3fe0bd4d2cb9bb2434737
kiberleab1 <kiberleabeniyew@gmail.com>
2024-09-02 06:06:49 +0300

    added build directory

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,4 +9,8 @@ export default defineConfig({
       src: "/src",
     },
   },
-})
+  build: {
+    outDir: "./build",
+    emptyOutDir: true, // also necessary
+  },
+});
