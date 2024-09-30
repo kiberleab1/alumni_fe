fdd4cf647b339a7a6f4e437769e0fb26b89dc7ba
z <zemelatmen@gmail.com>
2024-10-01 02:01:01 +0300

    chat image uploading

--- src/pages/user/chatConnections/Chat.jsx
+++ src/pages/user/chatConnections/Chat.jsx
@@ -1,134 +1,257 @@
 import React, { useState } from "react";
+import { useEffect } from "react";
+import { useRef } from "react";
 import { IoIosSend, IoMdArrowRoundBack } from "react-icons/io";
-
+import { MdOutlineAttachFile } from "react-icons/md";
 export default function ChatUi() {
   const [selectedChat, setSelectedChat] = useState(null);
   const [message, setMessage] = useState("");
+  console.log(message);
+
+  const [image, setImage] = useState(null);
+  const [visibleChats, setVisibleChats] = useState(10);
+  const messagesEndRef = useRef(null);
   const [chatHistory, setChatHistory] = useState({
-    Abebe: [
+    Alemu: [
       {
         sender: "Abebe",
         message: "Hey, how's it going?",
+        image: null,
         time: "5:30:00 PM",
         isReceived: true,
       },
       {
         sender: "You",
-        message: "Hi! I'm doing well, thanks. How about you?",
+        message: "fuck u",
+        image: null,
         time: "5:35:00 PM",
         isReceived: false,
       },
       {
         sender: "Abebe",
-        message:
-          "I'm great! Just wanted to catch up. Are you planning to attend the alumni event next month?",
-        time: "5:40:00 PM",
-        isReceived: true,
-      },
-      {
-        sender: "You",
-        message: "Hi! I'm doing well, thanks. How about you?",
-        time: "5:35:00 PM",
-        isReceived: false,
-      },
-      {
-        sender: "Abebe",
-        message:
-          "I'm great! Just wanted to catch up. Are you planning to attend the alumni event next month?",
-        time: "5:40:00 PM",
-        isReceived: true,
-      },
-      {
-        sender: "You",
-        message: "Hi! I'm doing well, thanks. How about you?",
-        time: "5:35:00 PM",
-        isReceived: false,
-      },
-      {
-        sender: "Abebe",
-        message:
-          "I'm great! Just wanted to catch up. Are you planning to attend the alumni event next month? Are you planning to attend the alumni event next month?",
-        time: "5:40:00 PM",
-        isReceived: true,
-      },
-      {
-        sender: "You",
-        message: "Hi! I'm doing well, thanks. How about you?",
-        time: "5:35:00 PM",
-        isReceived: false,
-      },
-      {
-        sender: "Abebe",
-        message:
-          "I'm great! Just wanted to catch up. Are you planning to attend the alumni event next month?",
-        time: "5:40:00 PM",
+        message: "Hey, how's it going?",
+        image: null,
+        time: "5:30:00 PM",
         isReceived: true,
       },
     ],
-    Alemu: [
+    Alx: [
       {
-        sender: "Alemu",
-        message: "Are you coming to the reunion?",
-        time: "4:30:00 PM",
+        sender: "Abebe",
+        message: "Hey, how's it going?",
+        image: null,
+        time: "5:30:00 PM",
+        isReceived: true,
+      },
+      {
+        sender: "You",
+        message: "fuck u",
+        image: null,
+        time: "5:35:00 PM",
+        isReceived: false,
+      },
+      {
+        sender: "Abebe",
+        message: "Hey, how's it going?",
+        image: null,
+        time: "5:30:00 PM",
         isReceived: true,
       },
     ],
-    Beruk: [
+    Abd: [
       {
-        sender: "Beruk",
-        message: "Great catching up yesterday!",
-        time: "3:30:00 PM",
+        sender: "Abebe",
+        message: "Hey, how's it going?",
+        image: null,
+        time: "5:30:00 PM",
+        isReceived: true,
+      },
+    ],
+    Abdw: [
+      {
+        sender: "Abebe",
+        message: "Hey, how's it going?",
+        image: null,
+        time: "5:30:00 PM",
+        isReceived: true,
+      },
+    ],
+    Abwd: [
+      {
+        sender: "Abebe",
+        message: "Hey, how's it going?",
+        image: null,
+        time: "5:30:00 PM",
+        isReceived: true,
+      },
+    ],
+    Awbd: [
+      {
+        sender: "Abebe",
+        message: "Hey, how's it going?",
+        image: null,
+        time: "5:30:00 PM",
+        isReceived: true,
+      },
+    ],
+    Abswd: [
+      {
+        sender: "Abebe",
+        message: "Hey, how's it going?",
+        image: null,
+        time: "5:30:00 PM",
+        isReceived: true,
+      },
+    ],
+    Abad: [
+      {
+        sender: "Abebe",
+        message: "Hey, how's it going?",
+        image: null,
+        time: "5:30:00 PM",
+        isReceived: true,
+      },
+    ],
+    Abvd: [
+      {
+        sender: "Abebe",
+        message: "Hey, how's it going?",
+        image: null,
+        time: "5:30:00 PM",
+        isReceived: true,
+      },
+    ],
+    Abawd: [
+      {
+        sender: "Abebe",
+        message: "Hey, how's it going?",
+        image: null,
+        time: "5:30:00 PM",
+        isReceived: true,
+      },
+    ],
+    Abvqd: [
+      {
+        sender: "Abebe",
+        message: "Hey, how's it going?",
+        image: null,
+        time: "5:30:00 PM",
         isReceived: true,
       },
     ],
   });
-
+  const chatUsers = Object.keys(chatHistory);
+  useEffect(() => {
+    if (messagesEndRef.current) {
+      messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [chatHistory[selectedChat]]);
   const onSendMessage = () => {
-    if (message.trim()) {
-      const newChat = [
-        ...chatHistory[selectedChat],
-        {
-          sender: "You",
-          message,
-          time: new Date().toLocaleTimeString(),
-          isReceived: false,
-        },
-      ];
-      setChatHistory({ ...chatHistory, [selectedChat]: newChat });
+    if (message.trim() || image) {
+      const messageData = {
+        message: message,
+        image: image,
+        time: new Date().toLocaleTimeString(),
+        sender: "You",
+      };
+      console.log(messageData.message);
+      setChatHistory((prevChatHistory) => ({
+        ...prevChatHistory,
+        [selectedChat]: [...(prevChatHistory[selectedChat] || []), messageData],
+      }));
+
       setMessage("");
+      setImage(null);
     }
   };
+  // const onSendMessage = () => {
+  //   if (message.trim()) {
+  //     setChatHistory((prev) => ({
+  //       ...prev,
+  //       [selectedChat]: [
+  //         ...prev[selectedChat],
+  //         {
+  //           sender: "You",
+  //           message,
+  //           image: image,
+  //           time: new Date().toLocaleTimeString(),
+  //           isReceived: false,
+  //         },
+  //       ],
+  //     }));
+  //     setMessage("");
+  //     setImage(null);
+  //   }
+  // };
+  const textareaRef = useRef(null);
+  useEffect(() => {
+    if (textareaRef.current) {
+      textareaRef.current.style.height = "auto"; // Reset height
+      textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`; // Set height to scrollHeight
+    }
+  }, [message]);
 
   const handleBackClick = () => {
     setSelectedChat(null);
   };
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter" && !e.shiftKey) {
+      e.preventDefault();
+      onSendMessage();
+    }
+  };
+
+  const handleFileUpload = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      const imageUrl = URL.createObjectURL(file);
+      setImage(imageUrl);
+    }
+  };
+
+  const handleRemoveImage = () => {
+    setImage(null);
+  };
+  console.log(chatHistory[selectedChat]);
 
   return (
-    <div className="flex h-fit lg:h-[800px]">
+    <div className="flex h-[90vh] bg-white">
       <div
-        className={`w-[100%] md:w-1/3 p-1 md:p-4 border-r  ${
+        className={`w-[100%] md:w-1/3 p-1 md:p-4 border-r overflow-y-scroll no-scrollbar ${
           selectedChat ? "hidden md:block" : "block"
         }`}
       >
-        <h2 className="text-xl font-bold mb-4">Connections</h2>
-        {Object.keys(chatHistory).map((name) => (
+        <h2 className="text-xl font-medium mb-4 border-b-2">Connections</h2>
+        {chatUsers.slice(0, visibleChats).map((name) => (
           <div
             key={name}
-            onClick={() => setSelectedChat(name)}
-            className={`p-3 mb-3 rounded-lg cursor-pointer ${
-              selectedChat === name ? "bg-gray-300" : "bg-white"
-            } hover:bg-gray-200`}
+            onClick={() => {
+              setSelectedChat(name);
+              setMessage("");
+            }}
+            className={`p-3 mb-2 rounded-lg cursor-pointer hover:bg-gray-200 ${
+              selectedChat === name ? "bg-gray-300" : "bg-gray-100 "
+            } `}
           >
-            <h3 className="font-semibold text-start">{name}</h3>
-            <p className="text-gray-600 text-start">
+            <h3 className="font-semibold text-gray-800 text-start">{name}</h3>
+            <p className="text-gray-500 text-start line-clamp-1">
               {chatHistory[name][chatHistory[name].length - 1]?.message}
             </p>
           </div>
         ))}
+
+        {visibleChats < chatUsers.length && (
+          <button
+            onClick={() => setVisibleChats((prev) => prev + 10)} //  10 more users on click
+            className="mt-4 px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 w-full"
+          >
+            See more
+          </button>
+        )}
       </div>
 
       <div
-        className={`w-full md:w-2/3 p-1 md:p-4 flex flex-col justify-between ${
+        className={`w-full md:w-2/3 p-1 md:p-4 flex flex-col justify-between  ${
           selectedChat ? "block" : "hidden md:block"
         }`}
       >
@@ -143,30 +266,97 @@ export default function ChatUi() {
                 <IoMdArrowRoundBack className="text-black" />
               </button>
             </div>
-            <div className="flex flex-col overflow-y-auto h-screen space-y-4">
-              {chatHistory[selectedChat].map((chat, index) => (
-                <div
-                  key={index}
-                  className={`flex  ${
-                    chat.isReceived ? "justify-start" : "justify-end"
-                  }`}
-                >
+
+            <div className="flex flex-col h-full overflow-y-scroll no-scrollbar">
+              <div className="flex-1 h- ">
+                {chatHistory[selectedChat]?.map((msg, index) => (
                   <div
-                    className={`px-3 py-1  sm:p-3 rounded-lg ${
-                      chat.isReceived
-                        ? "bg-gray-200 text-black max-w-[60%]"
-                        : "bg-black text-white"
+                    key={index}
+                    className={`p-2 ${
+                      msg.isReceived ? "text-left" : "text-right"
                     }`}
                   >
-                    <p className="text-start">{chat.message}</p>
-                    <span className="text-xs mt-1 block text-start">
-                      {chat.time}
+                    <div className={`inline-block max-w-[60%]`}>
+                      {msg.image && ( // Check if the message has an image
+                        <img
+                          src={msg.image}
+                          alt="Sent"
+                          className={`rounded-lg mb-1 ${
+                            msg.isReceived ? "bg-gray-300" : "bg-gray-800"
+                          }`}
+                        />
+                      )}
+                      {msg.message && ( // Check if the message has text
+                        <p
+                          className={`p-2 rounded-lg ${
+                            msg.isReceived
+                              ? "bg-gray-300 text-black"
+                              : "bg-gray-800 text-white"
+                          }`}
+                        >
+                          {msg.message}
+                        </p>
+                      )}
+                    </div>
+                    <span className="block text-xs mt-1 ml-1 text-gray-500">
+                      {msg.time}
                     </span>
                   </div>
+                ))}
+
+                <div ref={messagesEndRef} />
+              </div>
+              <div>
+                {/* Chat History */}
+
+                {/* Chat Input */}
+                <div className="flex items-center border-t p-2 bg-gray-100">
+                  <textarea
+                    ref={textareaRef}
+                    value={message}
+                    rows={1}
+                    onChange={(e) => setMessage(e.target.value)}
+                    onKeyDown={handleKeyDown}
+                    style={{ minHeight: "40px", maxHeight: "40px" }}
+                    placeholder="Type a message..."
+                    className="flex-1 p-2 border-none rounded-r-none w-[100%] rounded-l-lg bg-gray-100 text-black resize-none overflow-hidden no-scrollbar"
+                  />
+                  <div className="flex flex-row transform">
+                    <input
+                      type="file"
+                      accept="image/*"
+                      onChange={handleFileUpload}
+                      className="hidden"
+                      id="file-upload"
+                    />
+                    <label
+                      htmlFor="file-upload"
+                      className="cursor-pointer bg-inherit text-2xl  py-2 rounded-lg"
+                    >
+                      <MdOutlineAttachFile />
+                    </label>
+                    <button
+                      onClick={onSendMessage}
+                      className="bg-inherit text-2xl px-4 py-2 rounded-l-none"
+                    >
+                      <IoIosSend />
+                    </button>
+                  </div>
                 </div>
-              ))}
+              </div>
+              {image && ( // Show image if selected
+                <div className="relative">
+                  <img
+                    src={image}
+                    alt="Selected"
+                    className="w-12 h-12 object-cover rounded-full ml-2 cursor-pointer"
+                    onClick={handleRemoveImage} // Remove image on click
+                  />
+                </div>
+              )}
             </div>
-            <div className="flex items-center border-t ">
+
+            {/* <div className="flex items-center border-t ">
               <input
                 type="text"
                 value={message}
@@ -180,7 +370,7 @@ export default function ChatUi() {
               >
                 <IoIosSend />
               </button>
-            </div>
+            </div> */}
           </>
         )}
       </div>
