61df1737d7d6630ef682cdb9947c4559207c50aa
z <zemelatmen@gmail.com>
2024-10-01 11:53:53 +0300

    chat image uploding/ui

--- src/pages/user/chatConnections/Chat.jsx
+++ src/pages/user/chatConnections/Chat.jsx
@@ -1,40 +1,43 @@
 import React, { useState } from "react";
 import { useEffect } from "react";
 import { useRef } from "react";
+import { FaBinoculars } from "react-icons/fa";
 import { IoIosSend, IoMdArrowRoundBack } from "react-icons/io";
-import { MdOutlineAttachFile } from "react-icons/md";
+import { IoTelescope, IoWoman } from "react-icons/io5";
+import { MdOutlineAttachFile, MdTagFaces } from "react-icons/md";
+// import "./css.css";
 export default function ChatUi() {
   const [selectedChat, setSelectedChat] = useState(null);
   const [message, setMessage] = useState("");
-  console.log(message);
 
-  const [image, setImage] = useState(null);
+  const [image, setImage] = useState("");
+  console.log(image);
   const [visibleChats, setVisibleChats] = useState(10);
   const messagesEndRef = useRef(null);
   const [chatHistory, setChatHistory] = useState({
-    Alemu: [
-      {
-        sender: "Abebe",
-        message: "Hey, how's it going?",
-        image: null,
-        time: "5:30:00 PM",
-        isReceived: true,
-      },
-      {
-        sender: "You",
-        message: "fuck u",
-        image: null,
-        time: "5:35:00 PM",
-        isReceived: false,
-      },
-      {
-        sender: "Abebe",
-        message: "Hey, how's it going?",
-        image: null,
-        time: "5:30:00 PM",
-        isReceived: true,
-      },
-    ],
+    // Alemu: [
+    //   {
+    //     sender: "Abebe",
+    //     message: "Hey, how's it going?",
+    //     image: null,
+    //     time: "5:30:00 PM",
+    //     isReceived: true,
+    //   },
+    //   {
+    //     sender: "You",
+    //     message: "fuck u",
+    //     image: null,
+    //     time: "5:35:00 PM",
+    //     isReceived: false,
+    //   },
+    //   {
+    //     sender: "Abebe",
+    //     message: "Hey, how's it going?",
+    //     image: null,
+    //     time: "5:30:00 PM",
+    //     isReceived: true,
+    //   },
+    // ],
     Alx: [
       {
         sender: "Abebe",
@@ -58,87 +61,87 @@ export default function ChatUi() {
         isReceived: true,
       },
     ],
-    Abd: [
-      {
-        sender: "Abebe",
-        message: "Hey, how's it going?",
-        image: null,
-        time: "5:30:00 PM",
-        isReceived: true,
-      },
-    ],
-    Abdw: [
-      {
-        sender: "Abebe",
-        message: "Hey, how's it going?",
-        image: null,
-        time: "5:30:00 PM",
-        isReceived: true,
-      },
-    ],
-    Abwd: [
-      {
-        sender: "Abebe",
-        message: "Hey, how's it going?",
-        image: null,
-        time: "5:30:00 PM",
-        isReceived: true,
-      },
-    ],
-    Awbd: [
-      {
-        sender: "Abebe",
-        message: "Hey, how's it going?",
-        image: null,
-        time: "5:30:00 PM",
-        isReceived: true,
-      },
-    ],
-    Abswd: [
-      {
-        sender: "Abebe",
-        message: "Hey, how's it going?",
-        image: null,
-        time: "5:30:00 PM",
-        isReceived: true,
-      },
-    ],
-    Abad: [
-      {
-        sender: "Abebe",
-        message: "Hey, how's it going?",
-        image: null,
-        time: "5:30:00 PM",
-        isReceived: true,
-      },
-    ],
-    Abvd: [
-      {
-        sender: "Abebe",
-        message: "Hey, how's it going?",
-        image: null,
-        time: "5:30:00 PM",
-        isReceived: true,
-      },
-    ],
-    Abawd: [
-      {
-        sender: "Abebe",
-        message: "Hey, how's it going?",
-        image: null,
-        time: "5:30:00 PM",
-        isReceived: true,
-      },
-    ],
-    Abvqd: [
-      {
-        sender: "Abebe",
-        message: "Hey, how's it going?",
-        image: null,
-        time: "5:30:00 PM",
-        isReceived: true,
-      },
-    ],
+    // Abd: [
+    //   {
+    //     sender: "Abebe",
+    //     message: "Hey, how's it going?",
+    //     image: null,
+    //     time: "5:30:00 PM",
+    //     isReceived: true,
+    //   },
+    // ],
+    // Abdw: [
+    //   {
+    //     sender: "Abebe",
+    //     message: "Hey, how's it going?",
+    //     image: null,
+    //     time: "5:30:00 PM",
+    //     isReceived: true,
+    //   },
+    // ],
+    // Abwd: [
+    //   {
+    //     sender: "Abebe",
+    //     message: "Hey, how's it going?",
+    //     image: null,
+    //     time: "5:30:00 PM",
+    //     isReceived: true,
+    //   },
+    // ],
+    // Awbd: [
+    //   {
+    //     sender: "Abebe",
+    //     message: "Hey, how's it going?",
+    //     image: null,
+    //     time: "5:30:00 PM",
+    //     isReceived: true,
+    //   },
+    // ],
+    // Abswd: [
+    //   {
+    //     sender: "Abebe",
+    //     message: "Hey, how's it going?",
+    //     image: null,
+    //     time: "5:30:00 PM",
+    //     isReceived: true,
+    //   },
+    // ],
+    // Abad: [
+    //   {
+    //     sender: "Abebe",
+    //     message: "Hey, how's it going?",
+    //     image: null,
+    //     time: "5:30:00 PM",
+    //     isReceived: true,
+    //   },
+    // ],
+    // Abvd: [
+    //   {
+    //     sender: "Abebe",
+    //     message: "Hey, how's it going?",
+    //     image: null,
+    //     time: "5:30:00 PM",
+    //     isReceived: true,
+    //   },
+    // ],
+    // Abawd: [
+    //   {
+    //     sender: "Abebe",
+    //     message: "Hey, how's it going?",
+    //     image: null,
+    //     time: "5:30:00 PM",
+    //     isReceived: true,
+    //   },
+    // ],
+    // Abvqd: [
+    //   {
+    //     sender: "Abebe",
+    //     message: "Hey, how's it going?",
+    //     image: null,
+    //     time: "5:30:00 PM",
+    //     isReceived: true,
+    //   },
+    // ],
   });
   const chatUsers = Object.keys(chatHistory);
   useEffect(() => {
@@ -162,7 +165,7 @@ export default function ChatUi() {
       }));
 
       setMessage("");
-      setImage(null);
+      setImage("");
     }
   };
   // const onSendMessage = () => {
@@ -187,8 +190,8 @@ export default function ChatUi() {
   const textareaRef = useRef(null);
   useEffect(() => {
     if (textareaRef.current) {
-      textareaRef.current.style.height = "auto"; // Reset height
-      textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`; // Set height to scrollHeight
+      textareaRef.current.style.height = "auto";
+      textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`;
     }
   }, [message]);
 
@@ -223,23 +226,34 @@ export default function ChatUi() {
         }`}
       >
         <h2 className="text-xl font-medium mb-4 border-b-2">Connections</h2>
-        {chatUsers.slice(0, visibleChats).map((name) => (
-          <div
-            key={name}
-            onClick={() => {
-              setSelectedChat(name);
-              setMessage("");
-            }}
-            className={`p-3 mb-2 rounded-lg cursor-pointer hover:bg-gray-200 ${
-              selectedChat === name ? "bg-gray-300" : "bg-gray-100 "
-            } `}
-          >
-            <h3 className="font-semibold text-gray-800 text-start">{name}</h3>
-            <p className="text-gray-500 text-start line-clamp-1">
-              {chatHistory[name][chatHistory[name].length - 1]?.message}
-            </p>
+        {chatUsers.length < 1 ? (
+          <div className="flex flex-col  text-center gap-2  m-auto rounded-lg p-6 ">
+            <IoTelescope className="m-auto text-black text-4xl" />
+
+            <h1 className="text-gray-600 text-md">
+              {" "}
+              You have no Connections history
+            </h1>
           </div>
-        ))}
+        ) : (
+          chatUsers.slice(0, visibleChats).map((name) => (
+            <div
+              key={name}
+              onClick={() => {
+                setSelectedChat(name);
+                setMessage("");
+              }}
+              className={`p-3 mb-2 rounded-lg cursor-pointer hover:bg-gray-200 ${
+                selectedChat === name ? "bg-gray-300" : "bg-gray-100"
+              }`}
+            >
+              <h3 className="font-semibold text-gray-800 text-start">{name}</h3>
+              <p className="text-gray-500 text-start line-clamp-1 text-[.8rem]">
+                {chatHistory[name][chatHistory[name].length - 1]?.message}
+              </p>
+            </div>
+          ))
+        )}
 
         {visibleChats < chatUsers.length && (
           <button
@@ -256,7 +270,7 @@ export default function ChatUi() {
           selectedChat ? "block" : "hidden md:block"
         }`}
       >
-        {selectedChat && (
+        {selectedChat ? (
           <>
             <div className="flex justify-between items-center mb-4">
               <h2 className="text-2xl font-bold">{selectedChat}</h2>
@@ -268,17 +282,17 @@ export default function ChatUi() {
               </button>
             </div>
 
-            <div className="flex flex-col h-full overflow-y-scroll no-scrollbar">
+            <div className="flex flex-col h-full overflow-y-scroll no-scrollbar font-sans">
               <div className="flex-1 h- ">
                 {chatHistory[selectedChat]?.map((msg, index) => (
                   <div
                     key={index}
                     className={`p-2 ${
-                      msg.isReceived ? "text-left" : "text-right"
+                      msg.isReceived ? "text-left " : "text-right"
                     }`}
                   >
-                    <div className={`inline-block max-w-[60%]`}>
-                      {msg.image && ( // Check if the message has an image
+                    <div className={`inline-block max-w-[90% lg:max-w-[70%]`}>
+                      {msg.image && (
                         <img
                           src={msg.image}
                           alt="Sent"
@@ -287,12 +301,12 @@ export default function ChatUi() {
                           }`}
                         />
                       )}
-                      {msg.message && ( // Check if the message has text
+                      {msg.message && (
                         <p
-                          className={`p-2 rounded-lg ${
+                          className={`flex p-2 rounded-lg font-sans  ${
                             msg.isReceived
-                              ? "bg-gray-300 text-black"
-                              : "bg-gray-800 text-white"
+                              ? "bg-gray-300 text-black  text-[.9rem]  "
+                              : "bg-gray-900 text-white  text-[.9rem] text-left"
                           }`}
                         >
                           {msg.message}
@@ -308,12 +322,8 @@ export default function ChatUi() {
                 <div ref={messagesEndRef} />
               </div>
               <div>
-                {/* Chat History */}
-
-                {/* Chat Input */}
                 <div className="flex items-center border-t p-2 bg-gray-100">
                   <textarea
-                    type="text"
                     ref={textareaRef}
                     value={message}
                     rows={1}
@@ -346,13 +356,13 @@ export default function ChatUi() {
                   </div>
                 </div>
               </div>
-              {image && ( // Show image if selected
+              {image && (
                 <div className="relative">
                   <img
                     src={image}
                     alt="Selected"
                     className="w-12 h-12 object-cover rounded-full ml-2 cursor-pointer"
-                    onClick={handleRemoveImage} // Remove image on click
+                    onClick={handleRemoveImage}
                   />
                 </div>
               )}
@@ -374,6 +384,22 @@ export default function ChatUi() {
               </button>
             </div> */}
           </>
+        ) : (
+          <div className="relative text-center bg-inherit  bg-gray-100  h-full">
+            {/* <div className="dot w-4 h-4 bg-red-500 rounded-full animate-bounce"></div>
+            <div className="dot two w-4 h-4 bg-red-500 rounded-full animate-bounce ml-2"></div> */}
+            <div className="face2  mt-4">
+              <div className="sad mt-2 w-8 h-2  !border-none inline-block ">
+                <MdTagFaces className="text-8xl text-yellow-700 rotate-180" />
+              </div>
+            </div>
+            <div className="shadow move  inset-x-0 bottom-0 h-2 bg-gray-300 animate-pulse"></div>
+            <div className="message mt-4">
+              <p className="text-gray-600 text-lg">
+                You haven't selected a chat history.
+              </p>
+            </div>
+          </div>
         )}
       </div>
     </div>
